1f6bd849287dd6d1064361e100e42bfd0b860f62
Yasmine C. Fortes <yazzcastrof@gmail.com>
2023-09-14 00:47:49 -0300

    últimos ajustes

---
+++ src/components/ItemList/index.jsx
@@ -0,0 +1,17 @@
+import './styles.css';
+
+function ItemList({title, description}) {
+  return (
+    <div className="item-list">
+      <strong>
+        { title }
+      </strong>
+      <p>
+        { description}
+      </p>
+      <hr />
+    </div>
+  )
+}
+
+export { ItemList };
